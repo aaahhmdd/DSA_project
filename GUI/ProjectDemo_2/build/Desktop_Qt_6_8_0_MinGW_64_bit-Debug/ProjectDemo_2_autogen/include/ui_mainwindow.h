37f759c0e5bb19a4b3ae0e0de403c3c2e4c72776
Ahmed Sameh <138621729+aaahhmdd@users.noreply.github.com>
2024-12-08 21:46:13 +0200

    create project GUI demo

---
+++ GUI/ProjectDemo_2/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ProjectDemo_2_autogen/include/ui_mainwindow.h
@@ -0,0 +1,174 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QWidget *layoutWidget;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QTextEdit *textEdit;
+    QWidget *layoutWidget1;
+    QVBoxLayout *verticalLayout_2;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton;
+    QPushButton *pushButton_6;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_2;
+    QPushButton *pushButton_7;
+    QTextEdit *textEdit_2;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(599, 490);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        layoutWidget = new QWidget(centralwidget);
+        layoutWidget->setObjectName("layoutWidget");
+        layoutWidget->setGeometry(QRect(220, 243, 351, 191));
+        verticalLayout = new QVBoxLayout(layoutWidget);
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(layoutWidget);
+        label->setObjectName("label");
+
+        verticalLayout->addWidget(label);
+
+        textEdit = new QTextEdit(layoutWidget);
+        textEdit->setObjectName("textEdit");
+
+        verticalLayout->addWidget(textEdit);
+
+        layoutWidget1 = new QWidget(centralwidget);
+        layoutWidget1->setObjectName("layoutWidget1");
+        layoutWidget1->setGeometry(QRect(10, 10, 211, 421));
+        verticalLayout_2 = new QVBoxLayout(layoutWidget1);
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        pushButton_4 = new QPushButton(layoutWidget1);
+        pushButton_4->setObjectName("pushButton_4");
+
+        verticalLayout_2->addWidget(pushButton_4);
+
+        pushButton_5 = new QPushButton(layoutWidget1);
+        pushButton_5->setObjectName("pushButton_5");
+
+        verticalLayout_2->addWidget(pushButton_5);
+
+        pushButton_3 = new QPushButton(layoutWidget1);
+        pushButton_3->setObjectName("pushButton_3");
+
+        verticalLayout_2->addWidget(pushButton_3);
+
+        pushButton_2 = new QPushButton(layoutWidget1);
+        pushButton_2->setObjectName("pushButton_2");
+
+        verticalLayout_2->addWidget(pushButton_2);
+
+        pushButton = new QPushButton(layoutWidget1);
+        pushButton->setObjectName("pushButton");
+
+        verticalLayout_2->addWidget(pushButton);
+
+        pushButton_6 = new QPushButton(layoutWidget1);
+        pushButton_6->setObjectName("pushButton_6");
+
+        verticalLayout_2->addWidget(pushButton_6);
+
+        widget = new QWidget(centralwidget);
+        widget->setObjectName("widget");
+        widget->setGeometry(QRect(220, 7, 351, 231));
+        verticalLayout_3 = new QVBoxLayout(widget);
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        label_2 = new QLabel(widget);
+        label_2->setObjectName("label_2");
+
+        horizontalLayout->addWidget(label_2);
+
+        pushButton_7 = new QPushButton(widget);
+        pushButton_7->setObjectName("pushButton_7");
+
+        horizontalLayout->addWidget(pushButton_7);
+
+
+        verticalLayout_3->addLayout(horizontalLayout);
+
+        textEdit_2 = new QTextEdit(widget);
+        textEdit_2->setObjectName("textEdit_2");
+        textEdit_2->setReadOnly(true);
+
+        verticalLayout_3->addWidget(textEdit_2);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 599, 18));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Input XML Content", nullptr));
+        textEdit->setPlaceholderText(QCoreApplication::translate("MainWindow", "Paste or type your XML content here...", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "decompression", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "minifying", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "compression", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "To json", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "is_balanced", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "prettify", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Output", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("MainWindow", "save", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ GUI/ProjectDemo_2/xmltojson.h
@@ -0,0 +1,21 @@
+#ifndef XMLTOJSON_H
+#define XMLTOJSON_H
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+// Element struct definition
+struct Element {
+    std::string tag;
+    std::string value;
+    std::vector<Element> children;
+};
+
+// Function declarations
+Element parseXML(const std::string& xml);
+void toJSON(const Element& element, std::string& json, int indent = 0);
+
+#endif // XMLTOJSON_H
